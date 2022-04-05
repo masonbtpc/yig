bb46da646e9959efcec7d3d6ab4226273a9a781a
masonbtpc <mblachford@gmail.com>
2022-04-05 08:42:18 -0400

    fix: updated prepare_env.sh for cockroach

--- integrate/prepare_env.sh
+++ integrate/prepare_env.sh
@@ -4,14 +4,25 @@ function prepare_ceph(){
 	docker exec ceph ceph osd pool create turtle 32
 }
 
-function prepare_mysql(){
-	docker exec mysql mysql -e "create database yig character set utf8;"
-    docker exec mysql mysql -e "use yig;source /yig.sql;"
+function prepare_tidb(){
+    docker cp yig.sql tidb:/yig.sql
+    docker exec tidb apt update
+    docker exec tidb apt install default-mysql-client -y
+	docker exec tidb mysql -e "create database yig character set utf8;"
+    docker exec tidb mysql -e "use yig;source /yig.sql;"
 }
 
-function prepare_tidb(){
-	docker exec mysql mysql -P 4000 -h 10.5.0.17 -e "create database yig character set utf8;"
-    docker exec mysql mysql -P 4000 -h 10.5.0.17 -e "use yig;source /yig.sql;"
+function create_tidb(){
+	docker exec tidb mysql -P 4000 -e "create database yig character set utf8;"
+    docker exec tidb mysql -P 4000 -e "use yig;source /yig.sql;"
+}
+
+function prepare_cockroachdb() {
+    docker cp yig_pg.sql cockroachdb:/cockroach/yig_pg.sql
+}
+
+function configure_cockroachdb() {
+    docker exec cockroachdb bash -c "cockroach sql --insecure --database="yig" < /cockroach/yig_pg.sql" > /dev/null 2>&1
 }
 
 function prepare_vault(){
@@ -22,10 +33,13 @@ function prepare_vault(){
 
 echo "creating Ceph pool..."
 prepare_ceph
-echo "creating  MySQL db..."
-docker cp yig.sql mysql:/yig.sql
-prepare_mysql
-echo "creating TiDB db..."
+echo "preparing TiDB db..."
 prepare_tidb
+echo "creating TiDB db..."
+create_tidb
+echo "preparing CockroachDB db..."
+prepare_cockroachdb
+echo "creating CockroachDB db..."
+create_cockroachdb
 echo "creating Vault..."
 prepare_vault

79ea72c42f72987b9904327be7ebddc9e608eb87
masonbtpc <mblachford@gmail.com>
2022-04-19 13:00:53 -0400

    fix: updated build.sh with swtich statement per db

--- integrate/build.sh
+++ integrate/build.sh
@@ -1,3 +1,26 @@
 BASEDIR=$(dirname $(pwd))
 BUILDDIR=$1
+DATABASE=$2
+
+case $DATABASE in
+    cockroachdb)
+        echo "Building Yig with CockroachDB"
+        db_info="postgres://yig:Nthe8eez!@10.5.0.9:26257/yigdb"
+        meta_store="cockroachdb"
+        ;;
+    tidb)
+        echo "Building Yig with TiDB"
+        db_info="root:@tcp(10.5.0.17:4000)/yigdb"
+        meta_store="tidb"
+        ;;
+    * )
+        echo "Unknown database type specified. Defaulting to building Yig with CockroachDB"
+        db_info = "postgres://yig:Nthe8eez!@10.5.0.9:26257/yigdb"
+        meta_store = "cockroachdb"
+        ;;
+esac
+
+sed -i "s|db_info|db_info = \"$db_info\"|" $PWD/yigconf/yig.toml
+sed -i "s|meta_store|meta_store = \"$meta_store\"|" $PWD/yigconf/yig.toml
+
 sudo docker run --rm -v ${BASEDIR}:${BUILDDIR} -w ${BUILDDIR} journeymidnight/yig bash -c 'make build_internal'

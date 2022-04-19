eead3e1fd98643ff867db512524bc69fdf728b64
masonbtpc <mblachford@gmail.com>
2022-04-19 13:01:33 -0400

    fix: modfied env prep script to support multidb

--- integrate/prepare_env.sh
+++ integrate/prepare_env.sh
@@ -1,29 +1,38 @@
+DATABASE=$1
+
 function prepare_ceph(){
 	docker exec ceph ceph osd pool create tiger 32
 	docker exec ceph ceph osd pool create rabbit 32
 	docker exec ceph ceph osd pool create turtle 32
 }
 
-function prepare_tidb(){
-    docker cp yig.sql tidb:/yig.sql
-    docker exec tidb apk update > /dev/null 2>&1
-    docker exec tidb apk add mysql-client > /dev/null 2>&1
-}
-
-function create_tidb(){
-	docker exec tidb mysql -P 4000 -h 127.0.0.1 -e "create database yig character set utf8;"
-    docker exec tidb mysql -P 4000 -h 127.0.0.1 -e "use yig;source /yig.sql;"
-}
-
-function prepare_cockroachdb() {
-    docker cp yig_pg.sql cockroachdb:/cockroach/yig_pg.sql
-    docker exec cockroachdb bash -c "echo 'create user yig;' | cockroach sql --insecure" > /dev/null 2>&1
-    docker exec cockroachdb bash -c "echo 'create database yig;'| cockroach sql --insecure" > /dev/null 2>&1
-    docker exec cockroachdb bash -c "echo 'grant all on database yig to yig;'| cockroach sql --insecure" > /dev/null 2>&1 
-}
-
-function create_cockroachdb() {
-    docker exec cockroachdb bash -c "cockroach sql -u yig -d yig --insecure < /cockroach/yig_pg.sql" > /dev/null 2>&1
+function prepare_database(){
+    case $DATABASE in
+        cockroachdb)
+            echo "Preparing Yig for CockroachDB metastore"
+            # Prepare Cockroach Environment
+            docker cp sql/crdb.sql cockroachdb:/cockroach/crdb.sql
+            docker exec cockroachdb bash -c "echo 'create user yig;' | cockroach sql --insecure" > /dev/null 2>&1
+            docker exec cockroachdb bash -c "echo 'create database yigdb;'| cockroach sql --insecure" > /dev/null 2>&1
+            docker exec cockroachdb bash -c "echo 'grant all on database yigdb to yig;'| cockroach sql --insecure" > /dev/null 2>&1 
+            #Create CockroachDB     
+            docker exec cockroachdb bash -c "cockroach sql -u yig -d yigdb --insecure < /cockroach/crdb.sql" > /dev/null 2>&1
+            ;;
+        tidb)
+            echo "Preparing Yig for TiDB metastore"
+            # Prepare TiDB Environment
+            docker cp sql/tidb.sql tidb:/tidb.sql
+            docker exec tidb apk update > /dev/null 2>&1
+            docker exec tidb apk add mysql-client > /dev/null 2>&1
+            # Create TiDB
+            docker exec tidb mysql -P 4000 -h 127.0.0.1 -e "create database yigdb character set utf8;"
+            docker exec tidb mysql -P 4000 -h 127.0.0.1 -e "use yigdb;source /tidb.sql;"
+            ;;
+        * )
+            echo "Unknown database type specified. Please edit the Makefile. Exiting"
+            exit(1)
+            ;;
+    esac
 }
 
 function prepare_vault(){
@@ -34,13 +43,7 @@ function prepare_vault(){
 
 echo "creating Ceph pool..."
 prepare_ceph
-echo "preparing TiDB db..."
-prepare_tidb
-echo "creating TiDB db..."
-create_tidb
-echo "preparing CockroachDB db..."
-prepare_cockroachdb
-echo "creating CockroachDB db..."
-create_cockroachdb
+echo "creating Database..."
+prepare_database
 echo "creating Vault..."
 prepare_vault

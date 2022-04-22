51586b806040f20f2a625c8de80c73d8d24fc85c
masonbtpc <mblachford@gmail.com>
2022-04-21 21:53:59 -0400

    feat: build configures time format

--- integrate/build.sh
+++ integrate/build.sh
@@ -10,11 +10,13 @@ case $DATABASE in
         echo "Building Yig with CockroachDB"
         db_info="postgres://yig:"${DBPASS}"@10.5.0.17:26257/yigdb"
         meta_store="cockroachdb"
+        time_format="2006-01-02 15:04:05"
         ;;
     tidb)
         echo "Building Yig with TiDB"
         db_info="yig:"${DBPASS}"@tcp(10.5.0.17:4000)/yigdb"
         meta_store="tidb"
+        time_format="2006-01-02T15:04:05.000Z"
         ;;
     * )
         echo "Unknown database type specified. Defaulting to building Yig with CockroachDB"
@@ -26,9 +28,11 @@ esac
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sed -i "s|db_info = \"[^\"]*\"|db_info = \"$db_info\"|" $PWD/yigconf/yig.toml
     sed -i "s|meta_store = \"[^\"]*\"|meta_store = \"$meta_store\"|" $PWD/yigconf/yig.toml
+    sed -i "s|time_format = \"[^\"]*\"|time_format = \"$time_format\"|" $PWD/yigconf/yig.toml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "s|db_info = \"[^\"]*\"|db_info = \"$db_info\"|" $PWD/yigconf/yig.toml
     sed -i '' "s|meta_store = \"[^\"]*\"|meta_store = \"$meta_store\"|" $PWD/yigconf/yig.toml
+    sed -i '' "s|time_format = \"[^\"]*\"|time_format = \"$time_format\"|" $PWD/yigconf/yig.toml
 fi
 
-sudo docker run --rm -v ${BASEDIR}:${BUILDDIR} -w ${BUILDDIR} journeymidnight/yig bash -c 'make build_internal'
+#sudo docker run --rm -v ${BASEDIR}:${BUILDDIR} -w ${BUILDDIR} journeymidnight/yig bash -c 'make build_internal'
